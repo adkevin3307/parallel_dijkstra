1fc9c4287766044c8cee9ba6ab8c0b783de98bad
adkevin3307 <adkevin3307@gmail.com>
2020-11-17 19:28:16 +0800

    add file exist check

--- convert.sh
+++ convert.sh
@@ -5,11 +5,15 @@ if [[ $# != 1 ]]; then
     exit 1
 fi
 
-OUTPUT_FILE=${1/%gr/in}
+if [[ -f $1 ]]; then
+    OUTPUT_FILE=${1/%gr/in}
 
-echo Convert $1 to $OUTPUT_FILE
+    echo Convert $1 to $OUTPUT_FILE
 
-cat $1 | grep -P "^p" | awk '{print $3}' > $OUTPUT_FILE
-cat $1 | grep -P "^a" | awk '{printf("%d %d %d\n", $2 - 1, $3 - 1, $4)}' >> $OUTPUT_FILE
+    cat $1 | grep -P "^p" | awk '{print $3}' > $OUTPUT_FILE
+    cat $1 | grep -P "^a" | awk '{printf("%d %d %d\n", $2 - 1, $3 - 1, $4)}' >> $OUTPUT_FILE
 
-echo Convert Success
+    echo Convert Success
+else
+    echo Error: $1 does not exist
+fi
